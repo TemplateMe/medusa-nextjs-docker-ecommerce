d92f3fdc07e5c94dee89e05a7b777044d8d81d81
AzSumToshko <msamuil@abv.bg>
2025-11-21 18:51:15 +0200

    Product Feed XML generation plugin

--- apps/backend/src/api/middlewares.ts
+++ apps/backend/src/api/middlewares.ts
@@ -1,25 +1,37 @@
 import { 
-    defineMiddlewares,
-    validateAndTransformBody,
-  } from "@medusajs/framework/http"
-  import { PostStoreCreateWishlistItem } from "./store/customers/me/wishlists/items/validators"
-  import { UpsertPreorderVariantSchema } from "./admin/variants/[id]/preorders/route"
+  defineMiddlewares,
+  validateAndTransformBody,
+  validateAndTransformQuery,
+} from "@medusajs/framework/http"
+import { PostStoreCreateWishlistItem } from "./store/customers/me/wishlists/items/validators"
+import { UpsertPreorderVariantSchema } from "./admin/variants/[id]/preorders/route"
+import { z } from 'zod';
   
-  export default defineMiddlewares({
-    routes: [
-      {
-        matcher: "/store/customers/me/wishlists/items",
-        method: "POST",
-        middlewares: [
-          validateAndTransformBody(PostStoreCreateWishlistItem),
-        ],
-      },
-      {
-        matcher: "/admin/variants/:id/preorders",
-        method: "POST",
-        middlewares: [
-          validateAndTransformBody(UpsertPreorderVariantSchema),
-        ],
-      },
-    ],
-  })
+export default defineMiddlewares({
+  routes: [
+    {
+      matcher: "/store/customers/me/wishlists/items",
+      method: "POST",
+      middlewares: [
+        validateAndTransformBody(PostStoreCreateWishlistItem),
+      ],
+    },
+    {
+      matcher: "/admin/variants/:id/preorders",
+      method: "POST",
+      middlewares: [
+        validateAndTransformBody(UpsertPreorderVariantSchema),
+      ],
+    },
+    {
+    matcher: "/product-feed",
+    methods: ["GET"],
+    middlewares: [
+      validateAndTransformQuery(z.object({
+        currency_code: z.string(),
+        country_code: z.string(),
+      }), {})
+    ]
+  }
+  ],
+})
